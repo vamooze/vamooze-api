79388be710658d0d9122dd75793cb9fafec4b9a5
balogun biola <balogunbiola101@gmail.com>
2024-08-04 21:38:36 +0100

    chore: (add another super user with hashed password)

--- seeds/superadmin.ts
+++ seeds/superadmin.ts
@@ -1,21 +1,28 @@
 import { Knex } from "knex";
+import { hashPassword } from '@feathersjs/authentication-local';
 
 exports.seed = async function (knex: Knex): Promise<void> {
   const superAdmin = {
     role: 4,
-    email: "admin3@vamooze.com",
+    email: "admin4@vamooze.com",
     password: "vamooze123$",
     first_name: "admin2",
     last_name: "vamooze",
   };
 
+  // Hash the password
+  const hashedPassword = await hashPassword(superAdmin.password);
+
   await knex("users")
-    .insert(superAdmin)
+    .insert({
+      ...superAdmin,
+      password: hashedPassword,
+    })
     .onConflict("id")
     .merge({
       role: superAdmin.role,
       email: superAdmin.email,
-      password: superAdmin.password,
+      password: hashedPassword,
       first_name: superAdmin.first_name,
       last_name: superAdmin.last_name
     });
