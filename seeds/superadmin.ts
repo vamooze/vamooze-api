d603060dc58912dafbda6001788f39510208dc2f
balogun biola <balogunbiola101@gmail.com>
2024-08-04 21:15:36 +0100

    chore: (seed new super admin)

--- seeds/superadmin.ts
+++ seeds/superadmin.ts
@@ -1,11 +1,11 @@
 import { Knex } from "knex";
 
 exports.seed = async function (knex: Knex): Promise<void> {
-  const superAdmin = { role: 4, email: "admin@vamooze.com" }
+  const superAdmin = { role: 4, email: "admin2@vamooze.com", password: 'vamooze123$' }
 
     await knex("users")
       .insert(superAdmin)
       .onConflict("id")
-      .merge({ role: superAdmin.role, email: superAdmin.email });
+      .merge({ role: superAdmin.role, email: superAdmin.email, password: superAdmin.password });
   
 };
