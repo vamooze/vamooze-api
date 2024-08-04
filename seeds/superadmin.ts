93605309c5aa09b5f6274ac2e0eebb41fc15dd06
balogun biola <balogunbiola101@gmail.com>
2024-08-04 21:22:17 +0100

    chore: (add another super user)

--- seeds/superadmin.ts
+++ seeds/superadmin.ts
@@ -1,11 +1,22 @@
 import { Knex } from "knex";
 
 exports.seed = async function (knex: Knex): Promise<void> {
-  const superAdmin = { role: 4, email: "admin2@vamooze.com", password: 'vamooze123$' }
+  const superAdmin = {
+    role: 4,
+    email: "admin3@vamooze.com",
+    password: "vamooze123$",
+    first_name: "admin2",
+    last_name: "vamooze",
+  };
 
-    await knex("users")
-      .insert(superAdmin)
-      .onConflict("id")
-      .merge({ role: superAdmin.role, email: superAdmin.email, password: superAdmin.password });
-  
+  await knex("users")
+    .insert(superAdmin)
+    .onConflict("id")
+    .merge({
+      role: superAdmin.role,
+      email: superAdmin.email,
+      password: superAdmin.password,
+      first_name: superAdmin.first_name,
+      last_name: superAdmin.last_name
+    });
 };
