16102d67906843ac3dc82384f109bc7928028159
balogun biola <balogunbiola101@gmail.com>
2024-08-06 23:33:37 +0100

    chore(fix duplication error in dispatch signup)

--- src/services/business/business.ts
+++ src/services/business/business.ts
@@ -1,8 +1,8 @@
 // For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
-import { authenticate } from '@feathersjs/authentication'
+import { authenticate } from "@feathersjs/authentication";
 
-import { hooks as schemaHooks } from '@feathersjs/schema'
-import emailTemplates from '../../helpers/emailTemplates'
+import { hooks as schemaHooks } from "@feathersjs/schema";
+import emailTemplates from "../../helpers/emailTemplates";
 import {
   businessDataValidator,
   businessPatchValidator,
@@ -11,27 +11,26 @@ import {
   businessExternalResolver,
   businessDataResolver,
   businessPatchResolver,
-  businessQueryResolver
-} from './business.schema'
+  businessQueryResolver,
+} from "./business.schema";
 
-import type { Application } from '../../declarations'
-import { BusinessService, getOptions } from './business.class'
-import { businessPath, businessMethods } from './business.shared'
-import {Conflict, NotFound} from "@feathersjs/errors";
-import {OAuthTypes, Roles} from "../../interfaces/constants";
-import {getOtp, sendEmail} from "../../helpers/functions";
-import {logger} from "../../logger";
-import {createValidator} from "express-joi-validation";
+import type { Application } from "../../declarations";
+import { BusinessService, getOptions } from "./business.class";
+import { businessPath, businessMethods } from "./business.shared";
+import { Conflict, NotFound } from "@feathersjs/errors";
+import { OAuthTypes, Roles } from "../../interfaces/constants";
+import { getOtp, sendEmail } from "../../helpers/functions";
+import { logger } from "../../logger";
+import { createValidator } from "express-joi-validation";
 import Joi from "joi";
-import {Termii} from "../../helpers/termii";
+import { Termii } from "../../helpers/termii";
 
-export * from './business.class'
-export * from './business.schema'
+export * from "./business.class";
+export * from "./business.schema";
 
-const validator = createValidator({ passError: true, statusCode: 400 })
+const validator = createValidator({ passError: true, statusCode: 400 });
 
 const phoneRegex = /^\+?\d+$/;
-
 
 const joi_phone_number_validator = Joi.string()
   .pattern(phoneRegex)
@@ -39,10 +38,11 @@ const joi_phone_number_validator = Joi.string()
   // .max(15)
   .required()
   .messages({
-    'string.pattern.base': 'Phone number must be a number and can only start with "+"',
-    'string.min': 'Phone number must be at least 7 digits long',
+    "string.pattern.base":
+      'Phone number must be a number and can only start with "+"',
+    "string.min": "Phone number must be at least 7 digits long",
     // 'string.max': 'Phone number cannot be longer than 15 digits',
-    'any.required': 'Phone number is required'
+    "any.required": "Phone number is required",
   });
 
 const schemas = {
@@ -59,16 +59,16 @@ const schemas = {
   dispatch_signup: Joi.object().keys({
     first_name: Joi.string().required(),
     last_name: Joi.string().required(),
-    phone_number: joi_phone_number_validator
+    phone_number: joi_phone_number_validator,
   }),
   dispatch_login: Joi.object().keys({
-    phone_number: joi_phone_number_validator
+    phone_number: joi_phone_number_validator,
   }),
   complete_dispatch_login: Joi.object().keys({
     phone_number: joi_phone_number_validator,
-    otp: Joi.number().required()
-  })
-}
+    otp: Joi.number().required(),
+  }),
+};
 
 // A configure function that registers the service and its hooks via `app.configure`
 export const business = (app: Application) => {
@@ -77,155 +77,205 @@ export const business = (app: Application) => {
     // A list of all methods this service exposes externally
     methods: businessMethods,
     // You can add additional custom events to be sent to clients here
-    events: []
-  })
-  app.post('/business/whitelabel/signup',  validator.body(schemas.signup), async (req: any, res: any, next: any) => {
-    try {
-      const user = await app.service('users').find({ query: { email: req.body.email } });
-
-      if(user?.data?.length > 0) {
-        throw new Conflict('User with this email already exists');
-      }
-      const role = await app.service('roles').find({query: { $limit: 1,slug: Roles.BusinessOwner}});
-      if (role?.data?.length === 0) {
-        throw new NotFound('Role does not exist');
-      }
-      req.body.role = role?.data[0]?.id;
-      req.body.otp = getOtp();
-      const result = await app.service('users').create(req.body);
-      res.json(result);
-    } catch (error: any) {
-      logger.error({message: error.message, stack: error.stack, email: req.body.email});
-      next(error);
-    }
+    events: [],
   });
+  app.post(
+    "/business/whitelabel/signup",
+    validator.body(schemas.signup),
+    async (req: any, res: any, next: any) => {
+      try {
+        const user = await app
+          .service("users")
+          .find({ query: { email: req.body.email } });
 
-  app.post('/dispatch/signup',  validator.body(schemas.dispatch_signup), async (req: any, res: any, next: any) => {
-    try {
-   
-      const user = await app.service('users').find({ query: { phone_number: req.body.phone_number } });
-
-      if(user?.data?.length > 0) {
-        throw new Conflict('User with this phone number already exists');
-      }
-
-      const role = await app.service('roles').find({query: { $limit: 1,slug: Roles.Dispatch}});
-      if (role?.data?.length === 0) {
-        throw new NotFound('Role does not exist');
-      }
-      req.body.role = role?.data[0]?.id;
-      req.body.otp = getOtp();
-      req.body.password = Roles.Dispatch;
-      const result = await app.service('users').create(req.body);
-   
-      // sendEmail({
-      //   toEmail: 'balogunbiola101@gmail.com',
-      //   subject: 'Testing email template on prod',
-      //   templateData: emailTemplates.otp(req.body.otp),
-      //   receiptName: 'biola balogun'
-      // })
-
-      const instance = new Termii(req.body.phone_number, `Your OTP is ${req.body.otp}`);
-      await instance.sendSMS();
-      res.json(result);
-    } catch (error: any) {
-      logger.error({message: error.message, stack: error.stack, phone_number: req.body.phone_number});
-      next(error);
-    }
-  });
-
-  app.post('/auth/dispatch/initiate-login', validator.body(schemas.dispatch_login), async (req: any, res: any) => {
-    try {
-      const User = app.service('users')
-      const userDetails = await User.find({
-        query: {
-          phone_number: req.body.phone_number
+        if (user?.data?.length > 0) {
+          throw new Conflict("User with this email already exists");
         }
-      })
-
-      if (userDetails?.data.length === 0) {
-        throw new NotFound('Ouch! User not found')
+        const role = await app
+          .service("roles")
+          .find({ query: { $limit: 1, slug: Roles.BusinessOwner } });
+        if (role?.data?.length === 0) {
+          throw new NotFound("Role does not exist");
+        }
+        req.body.role = role?.data[0]?.id;
+        req.body.otp = getOtp();
+        const result = await app.service("users").create(req.body);
+        res.json(result);
+      } catch (error: any) {
+        logger.error({
+          message: error.message,
+          stack: error.stack,
+          email: req.body.email,
+        });
+        next(error);
       }
-
-      req.body.otp = getOtp();
-      const result = await app.service('users').patch(userDetails?.data[0]?.id, {otp: req.body.otp});
-      const instance = new Termii(req.body.phone_number, `Your OTP is ${req.body.otp}`);
-      await instance.sendSMS();
-      console.log('===========================================req.body');
-      console.log(req.body);
-      console.log('===========================================req.body');
-      res.json({status: 200, message: "Otp sent successfully"});
-      // const data = await app
-      //     .service('authentication')
-      //     .create({ password: Roles.Dispatch, phone_number: req.body.phone_number, strategy: 'phone' })
-      // return res.json({ status: 200, data })
-    } catch (error) {
-      res.json(error)
     }
-  })
+  );
 
-  app.post('/auth/dispatch/complete-login', validator.body(schemas.complete_dispatch_login), async (req: any, res: any) => {
-    try {
-      const User = app.service('users')
-      const userDetails = await User.find({
-        query: {
+  app.post(
+    "/dispatch/signup",
+    validator.body(schemas.dispatch_signup),
+    async (req: any, res: any, next: any) => {
+      try {
+        const phoneNumber = req.body.phone_number.startsWith("+")
+          ? req.body.phone_number.slice(1)
+          : req.body.phone_number;
+
+        // Update the req.body.phone_number with the stripped phone number
+        req.body.phone_number = phoneNumber;
+
+        const user = await app
+          .service("users")
+          .find({ query: { phone_number: req.body.phone_number } });
+
+        if (user?.data?.length > 0) {
+          throw new Conflict("User with this phone number already exists");
+        }
+
+        const role = await app
+          .service("roles")
+          .find({ query: { $limit: 1, slug: Roles.Dispatch } });
+        if (role?.data?.length === 0) {
+          throw new NotFound("Role does not exist");
+        }
+        req.body.role = role?.data[0]?.id;
+        req.body.otp = getOtp();
+        req.body.password = Roles.Dispatch;
+        const result = await app.service("users").create(req.body);
+
+        // sendEmail({
+        //   toEmail: 'balogunbiola101@gmail.com',
+        //   subject: 'Testing email template on prod',
+        //   templateData: emailTemplates.otp(req.body.otp),
+        //   receiptName: 'biola balogun'
+        // })
+
+        const instance = new Termii(
+          req.body.phone_number,
+          `Your OTP is ${req.body.otp}`
+        );
+        await instance.sendSMS();
+        res.json(result);
+      } catch (error: any) {
+        logger.error({
+          message: error.message,
+          stack: error.stack,
           phone_number: req.body.phone_number,
-          otp: req.body.otp
-        }
-      })
-
-      if (userDetails?.data.length === 0) {
-        throw new NotFound('Ouch! Otp is incorrect')
+        });
+        next(error);
       }
-
-      await app.service('users').patch(userDetails?.data[0]?.id, {otp: 0});
-
-      const data = await app
-          .service('authentication')
-          .create({ password: Roles.Dispatch, phone_number: req.body.phone_number, strategy: 'phone' })
-      return res.json({ status: 200, data })
-    } catch (error) {
-      res.json(error)
     }
-  })
+  );
+
+  app.post(
+    "/auth/dispatch/initiate-login",
+    validator.body(schemas.dispatch_login),
+    async (req: any, res: any) => {
+      try {
+        const User = app.service("users");
+        const userDetails = await User.find({
+          query: {
+            phone_number: req.body.phone_number,
+          },
+        });
+
+        if (userDetails?.data.length === 0) {
+          throw new NotFound("Ouch! User not found");
+        }
+
+        req.body.otp = getOtp();
+        const result = await app
+          .service("users")
+          .patch(userDetails?.data[0]?.id, { otp: req.body.otp });
+        const instance = new Termii(
+          req.body.phone_number,
+          `Your OTP is ${req.body.otp}`
+        );
+        await instance.sendSMS();
+        console.log("===========================================req.body");
+        console.log(req.body);
+        console.log("===========================================req.body");
+        res.json({ status: 200, message: "Otp sent successfully" });
+        // const data = await app
+        //     .service('authentication')
+        //     .create({ password: Roles.Dispatch, phone_number: req.body.phone_number, strategy: 'phone' })
+        // return res.json({ status: 200, data })
+      } catch (error) {
+        res.json(error);
+      }
+    }
+  );
+
+  app.post(
+    "/auth/dispatch/complete-login",
+    validator.body(schemas.complete_dispatch_login),
+    async (req: any, res: any) => {
+      try {
+        const User = app.service("users");
+        const userDetails = await User.find({
+          query: {
+            phone_number: req.body.phone_number,
+            otp: req.body.otp,
+          },
+        });
+
+        if (userDetails?.data.length === 0) {
+          throw new NotFound("Ouch! Otp is incorrect");
+        }
+
+        await app.service("users").patch(userDetails?.data[0]?.id, { otp: 0 });
+
+        const data = await app
+          .service("authentication")
+          .create({
+            password: Roles.Dispatch,
+            phone_number: req.body.phone_number,
+            strategy: "phone",
+          });
+        return res.json({ status: 200, data });
+      } catch (error) {
+        res.json(error);
+      }
+    }
+  );
   // Initialize hooks
   app.service(businessPath).hooks({
     around: {
       all: [
-        authenticate('jwt'),
+        authenticate("jwt"),
         schemaHooks.resolveExternal(businessExternalResolver),
-        schemaHooks.resolveResult(businessResolver)
-      ]
+        schemaHooks.resolveResult(businessResolver),
+      ],
     },
     before: {
       all: [
         schemaHooks.validateQuery(businessQueryValidator),
-        schemaHooks.resolveQuery(businessQueryResolver)
+        schemaHooks.resolveQuery(businessQueryResolver),
       ],
       find: [],
       get: [],
       create: [
         schemaHooks.validateData(businessDataValidator),
-        schemaHooks.resolveData(businessDataResolver)
+        schemaHooks.resolveData(businessDataResolver),
       ],
       patch: [
         schemaHooks.validateData(businessPatchValidator),
-        schemaHooks.resolveData(businessPatchResolver)
+        schemaHooks.resolveData(businessPatchResolver),
       ],
-      remove: []
+      remove: [],
     },
     after: {
-      all: []
+      all: [],
     },
     error: {
-      all: []
-    }
-  })
-}
+      all: [],
+    },
+  });
+};
 
 // Add this service to the service type index
-declare module '../../declarations' {
+declare module "../../declarations" {
   interface ServiceTypes {
-    [businessPath]: BusinessService
+    [businessPath]: BusinessService;
   }
 }
