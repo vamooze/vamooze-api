9986cecf08766053cb5a3c3b35786a2acc86dc10
balogun biola <balogunbiola101@gmail.com>
2024-08-15 17:50:37 +0100

    chore: (add mobile_money_number)

--- migrations/20240814101505_dispatch.ts
+++ migrations/20240814101505_dispatch.ts
@@ -24,6 +24,8 @@ export async function up(knex: Knex): Promise<void> {
     table.string('next_of_kin_relationship').nullable();
     table.string('bank_account_name').nullable();
     table.string('bank_account_number').nullable();
+    table.string(' mobile_money_number').nullable();
+
     table.boolean('onboarding_quiz_completed').notNullable();
     table.boolean('suspended').notNullable().defaultTo(false);;
     table.timestamp('suspended_at').nullable();
