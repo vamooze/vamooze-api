f647ceba3426f6fecca3fe7062a04e6e9eb8319d
balogun biola <balogunbiola101@gmail.com>
2024-09-18 09:14:53 +0100

    add new migration

--- migrations/20240918075724_add_timezone_to_user_fields.ts
+++ migrations/20240918075724_add_timezone_to_user_fields.ts
@@ -3,13 +3,18 @@ import type { Knex } from 'knex'
 
 const tableName = 'users';
 export async function up(knex: Knex): Promise<void> {
-  await knex.schema.createTable(tableName, (table) => {
+    await knex.schema.alterTable(tableName, (table) => {
     table.boolean('is_inhouse_invitee_default_password').nullable()
-    table.bigInteger('in_house_inviter').references('id').inTable('users')
+    table.bigInteger('in_house_inviter').references('id').inTable(tableName)
     table.string('timezone').nullable()
   }) 
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable(tableName)
+    await knex.schema.alterTable(tableName, (table) => {
+        table.boolean('is_inhouse_invitee_default_password').nullable()
+        table.bigInteger('in_house_inviter').references('id').inTable(tableName)
+        table.string('timezone').nullable()
+    });
+
 }
