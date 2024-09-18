0d1a4dd39673d5564e38d25619c351d8c8015f19
balogun biola <balogunbiola101@gmail.com>
2024-09-18 09:08:45 +0100

    chore: (add migration file)

---
+++ migrations/20240918075724_add_timezone_to_user_fields.ts
@@ -0,0 +1,19 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/knexfile.html
+import type { Knex } from 'knex'
+
+const tableName = 'users';
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(tableName, (table) => {
+   
+    table.boolean('is_inhouse_invitee_default_password').nullable()
+    table.timestamp('last_password_change').nullable()
+    table.bigInteger('in_house_inviter').references('id').inTable('users')
+    table.string('timezone').nullable()
+  })
+
+ 
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable(tableName)
+}
